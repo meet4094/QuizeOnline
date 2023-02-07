ae2ce2a92fee1c8376d7396c9d97f474b46d9455
meet4094 <98247784+meet4094@users.noreply.github.com>
2023-02-07 19:05:28 +0530

    update

--- resources/views/apicall.blade.php
+++ resources/views/apicall.blade.php
@@ -8,7 +8,7 @@
 
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
-    <meta name="description" content="Video Downloader -  Admin Panel">
+    <meta name="description" content="QuizOnline -  Admin Panel">
     <meta name="author" content="Spruko Technologies Private Limited">
     {{-- <meta name="keywords"
         content="sales dashboard, admin dashboard, bootstrap 4 admin template, html admin template, admin panel design, admin panel design, bootstrap 4 dashboard, admin panel template, html dashboard template, bootstrap admin panel, sales dashboard design, best sales dashboards, sales performance dashboard, html5 template, dashboard template"> --}}
